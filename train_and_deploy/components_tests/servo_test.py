c8b0da0d2ebdbffc901af3a1d2847da049ccb830
HarrisonBounds <harrison.bounds777@gmail.com>
2023-09-01 10:04:36 -0500

    new servo test

--- train_and_deploy/components_tests/servo_test.py
+++ train_and_deploy/components_tests/servo_test.py
@@ -1,15 +1,37 @@
-from gpiozero import Servo
-from time import sleep
+import  RPi.GPIO as GPIO
+import time
 
-servo = Servo(25)
+GPIO.setmode(GPIO.BOARD)
 
-try:
-    while True:
-        servo.min()
-        sleep(0.5)
-        servo.mid()
-        sleep(0.5)
-        servo.max()
-        sleep(0.5)
-except KeyboardInterrupt:
-	print("Program stopped")
+GPIO.setup(11, GPIO.OUT)
+servo1 = GPIO.PWM(11, 50)
+
+servo1.start(0)
+print("Waiting")
+time.sleep(2)
+
+print("Rotating 180 degrees")
+
+duty = 2
+
+while duty <= 12:
+    servo1.ChangeDutyCycle(duty)
+    time.sleep(1)
+    duty = duty + 1
+
+time.sleep(2)
+
+print("Turning back to 90 degrees")
+servo1.ChangeDutyCycle(7)
+time.sleep(2)
+
+print("Turning back to 0 degrees")
+servo1.ChangeDutyCycle(2)
+time.sleep(0.5)
+servo1.ChangeDutyCycle(0)
+
+servo1.stop()
+GPIO.cleanup()
+print("Done")
+
+
