11a8bb02b675e395c8209ac6ae305311f9d76a12
HarrisonBounds <harrison.bounds777@gmail.com>
2023-09-05 15:19:04 -0500

    motor js component test

---
+++ train_and_deploy/components_tests/drive_js.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import json
+import pygame
+from pygame.locals import JOYAXISMOTION
+from pygame import event
+import motor
+
+config_path = os.path.join(sys.path[0], "train_and_deploy/config.json")
+f = open(config_path)
+data = json.load(f)
+throttle_lim = data['throttle_lim']
+
+pygame.joystick.init()
+js = pygame.joystick.Joystick(0)
+throttle = 0.
+
+try:
+    while True:
+        for e in event.get():
+            if e.type == JOYAXISMOTION:
+                throttle = -round((js.get_axis(1)), 2)  # throttle input: -1: max forward, 1: max backward
+                motor.drive(throttle * throttle_lim)  # apply throttle limit
+
+except KeyboardInterrupt:
+    motor.kill()
+    pygame.quit()
+    sys.exit()
+
+
