af8db0c22faaca7e2c5108239336433107e35ba9
HarrisonBounds <harrison.bounds777@gmail.com>
2023-09-05 16:42:15 -0500

    motor test with no pygame

--- train_and_deploy/drive_test.py
+++ train_and_deploy/drive_test.py
@@ -1,31 +1,48 @@
-import os
-import sys
-import json
-import pygame
-from pygame.locals import JOYAXISMOTION
-from pygame import event
-import motor
+import evdev
+import RPi.GPIO as GPIO
 
-config_path = os.path.join(sys.path[0], "config.json")
-f = open(config_path)
-data = json.load(f)
-throttle_lim = data['throttle_lim']
+# GPIO pins for motor control
+motor_pwm_pin = 18  # Replace with your PWM pin
+motor_direction_pin1 = 23  # Replace with your direction pin
+motor_direction_pin2 = 24  # Replace with your direction pin
 
-pygame.joystick.init()
-pygame.display.init()
-js = pygame.joystick.Joystick(0)
-throttle = 0.
+# Motor initialization
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(motor_pwm_pin, GPIO.OUT)
+GPIO.setup(motor_direction_pin1, GPIO.OUT)
+GPIO.setup(motor_direction_pin2, GPIO.OUT)
+
+# PWM setup for motor speed control
+motor_pwm = GPIO.PWM(motor_pwm_pin, 100)  # 100 Hz PWM frequency
+motor_pwm.start(0)  # Start with 0% duty cycle (stopped)
+
+# Replace '/dev/input/eventX' with the actual device path for your controller
+device_path = '/dev/input/event0'  # Modify this with your controller's device path
+
+def map_range(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 try:
-    while True:
-        for e in event.get():
-            if e.type == JOYAXISMOTION:
-                throttle = -round((js.get_axis(1)), 2)  # throttle input: -1: max forward, 1: max backward
-                motor.drive(throttle * throttle_lim)  # apply throttle limit
+    device = evdev.InputDevice(device_path)
+    print(f"Reading input events from {device.name}...")
 
+    for event in device.read_loop():
+        if event.type == evdev.ecodes.EV_ABS:
+            axis_event = evdev.ecodes.ABS[event.code]
+            axis_value = event.value
+
+            # Map the axis value to motor speed (0% to 100%)
+            speed = int(map_range(axis_value, -32768, 32767, 0, 100))
+            motor_pwm.ChangeDutyCycle(speed)
+
+        elif event.type == evdev.ecodes.EV_KEY:
+            # Handle button presses/releases if needed
+            pass
+
+except FileNotFoundError:
+    print(f"Device not found at {device_path}")
 except KeyboardInterrupt:
-    motor.kill()
-    pygame.quit()
-    sys.exit()
-
-
+    pass
+finally:
+    motor_pwm.stop()
+    GPIO.cleanup()
