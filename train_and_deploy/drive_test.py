2e61ff7d607ab0cbe14a6bfbddc501308547a6a5
HarrisonBounds <harrison.bounds777@gmail.com>
2023-09-05 16:48:52 -0500

    bug fixes

--- train_and_deploy/drive_test.py
+++ train_and_deploy/drive_test.py
@@ -25,19 +25,19 @@ def map_range(x, in_min, in_max, out_min, out_max):
 try:
     device = evdev.InputDevice(device_path)
     print(f"Reading input events from {device.name}...")
+    while True:
+        for event in device.read_loop():
+            if event.type == evdev.ecodes.EV_ABS:
+                axis_event = evdev.ecodes.ABS[event.code]
+                axis_value = event.value
 
-    for event in device.read_loop():
-        if event.type == evdev.ecodes.EV_ABS:
-            axis_event = evdev.ecodes.ABS[event.code]
-            axis_value = event.value
+                # Map the axis value to motor speed (0% to 100%)
+                speed = int(map_range(axis_value, -32768, 32767, 0, 100))
+                motor_pwm.ChangeDutyCycle(speed)
 
-            # Map the axis value to motor speed (0% to 100%)
-            speed = int(map_range(axis_value, -32768, 32767, 0, 100))
-            motor_pwm.ChangeDutyCycle(speed)
-
-        elif event.type == evdev.ecodes.EV_KEY:
-            # Handle button presses/releases if needed
-            pass
+            elif event.type == evdev.ecodes.EV_KEY:
+                # Handle button presses/releases if needed
+                pass
 
 except FileNotFoundError:
     print(f"Device not found at {device_path}")
