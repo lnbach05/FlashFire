6f21dbbf55708ac084c9bb362b215f27e37b3477
HarrisonBounds <harrison.bounds777@gmail.com>
2023-09-05 16:54:05 -0500

    new joystick test

--- train_and_deploy/components_tests/joystick_test.py
+++ train_and_deploy/components_tests/joystick_test.py
@@ -1,18 +1,34 @@
-import pygame
-from pygame.locals import *
-from pygame import event, display, joystick
+import evdev
+
+device_path = '/dev/input/event0'  # Modify this with your controller's device path
+
+def map_range(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+try:
+    device = evdev.InputDevice(device_path)
+    print(f"Reading input events from {device.name}...")
+    while True:
+        for event in device.read_loop():
+            if event.type == evdev.ecodes.EV_ABS:
+                axis_event = evdev.ecodes.ABS[event.code]
+                axis_value = event.value
+
+                print(f"Axis Value: {axis_value}")
+
+                # Map the axis value to motor speed (0% to 100%)
+                speed = int(map_range(axis_value, -32768, 32767, 0, 100))
+
+                print(f"Speed: {speed}")
+
+            elif event.type == evdev.ecodes.EV_KEY:
+                # Handle button presses/releases if needed
+                pass
+
+except FileNotFoundError:
+    print(f"Device not found at {device_path}")
+except KeyboardInterrupt:
+    pass
 
 
-def get_numControllers():
-    return joystick.get_count()
-
-pygame.init()
-display.init()
-joystick.init()
-print(f"{get_numControllers()} joystick connected")
-js = joystick.Joystick(0)
-while True:
-    for e in event.get():
-        if e.type == JOYAXISMOTION:
-            print(e)
 
