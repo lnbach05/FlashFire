7cd45ddf655cb06233f279c57ca0ac899c7e863b
HarrisonBounds <harrison.bounds777@gmail.com>
2023-09-07 14:06:21 -0500

    bug fix

--- train_and_deploy/drive_test.py
+++ train_and_deploy/drive_test.py
@@ -35,8 +35,8 @@ try:
                     axis_value = event.value
                     servo_angle = int(map_range(event.value, 0, 255, 0, 180)) #turning
                     servo_pwm.ChangeDutyCycle(servo_angle)
-                    
-                elif event.code == 3: #Y-axis of the right joystick (motor control)
+
+                elif event.code == 1: #Y-axis of the right joystick (motor control)
                     axis_event = evdev.ecodes.ABS[event.code]
                     axis_value = event.value
 
