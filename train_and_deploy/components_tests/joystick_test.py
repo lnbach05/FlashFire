2ce0f56738c1c8122a2902ecec95a149cb14d9b3
HarrisonBounds <harrison.bounds777@gmail.com>
2023-09-07 13:40:58 -0500

    joystick pwm range update

--- train_and_deploy/components_tests/joystick_test.py
+++ train_and_deploy/components_tests/joystick_test.py
@@ -17,7 +17,7 @@ try:
                 print(f"Axis Value: {axis_value}")
 
                 # Map the axis value to motor speed (0% to 100%)
-                speed = int(map_range(axis_value, 0, 128, 0, 80))
+                speed = int(map_range(axis_value, 255, 128, -80, 80))
 
                 print(f"Speed: {speed}")
 
