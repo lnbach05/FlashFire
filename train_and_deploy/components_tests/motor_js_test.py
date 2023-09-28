4cc4b2d82ce7aaee5c17594a9b2a9ae8e31892c0
HarrisonBounds <harrison.bounds777@gmail.com>
2023-09-28 13:29:08 -0500

    testing motor only

--- train_and_deploy/components_tests/motor_js_test.py
+++ train_and_deploy/components_tests/motor_js_test.py
@@ -1,8 +1,23 @@
 from adafruit_servokit import ServoKit
 import pygame
-import motor
 import os
 #import servo
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# AN1 = 26
+# DIG1 = 19
+PWM_PIN = 26
+DIR_PIN = 19
+
+GPIO.setup(PWM_PIN, GPIO.OUT)
+GPIO.setup(DIR_PIN, GPIO.OUT)
+
+# p1 = GPIO.PWM(AN1, 100)
+pwm = GPIO.PWM(PWM_PIN, 1000)
+pwm.start(0)
 
 #import this to run with pygame and no webcam
 os.environ["SDL_VIDEODRIVER"] = "dummy"
@@ -20,8 +35,17 @@ try:
         for e in pygame.event.get():
             if e.type == pygame.JOYAXISMOTION:
                 throttle = -js.get_axis(1)  # throttle input: -1: max forward, 1: max backward
+        if throttle > 0:
+            GPIO.output(DIR_PIN, GPIO.LOW)  # forward
+            pwm.ChangeDutyCycle(throttle)
+        elif throttle < 0:
+            GPIO.output(DIR_PIN, GPIO.HIGH)  # backward
+            throttle = -throttle
+            pwm.ChangeDutyCycle(throttle)
+        else:
+            pwm.ChangeDutyCycle(0)
                 #steer = -js.get_axis(3)  # steer_input: -1: left, 1: right
-        motor.drive(throttle)  # apply throttle limit
+       
         # ang = 90 * (1 + steer)
         # if ang > 180:
         #     ang = 180
