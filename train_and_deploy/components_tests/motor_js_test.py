232a9f73a9684f6aa1c393c697d7c635c6e8e600
HarrisonBounds <harrison.bounds777@gmail.com>
2023-09-28 13:42:06 -0500

    using gpiozero phase enable motor for simple motor use

--- train_and_deploy/components_tests/motor_js_test.py
+++ train_and_deploy/components_tests/motor_js_test.py
@@ -2,23 +2,7 @@ from adafruit_servokit import ServoKit
 import pygame
 import os
 #import servo
-import RPi.GPIO as GPIO
-import gpiozero
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-# AN1 = 26
-# DIG1 = 19
-PWM_PIN = 26
-DIR_PIN = 19
-
-GPIO.setup(PWM_PIN, GPIO.OUT)
-GPIO.setup(DIR_PIN, GPIO.OUT)
-
-# p1 = GPIO.PWM(AN1, 100)
-pwm = GPIO.PWM(PWM_PIN, 1000)
-pwm.start(0)
+from gpiozero import PhaseEnableMotor
 
 #import this to run with pygame and no webcam
 os.environ["SDL_VIDEODRIVER"] = "dummy"
@@ -32,28 +16,18 @@ throttle, steer = 0., 0.
 
 # MAIN
 try:
+    motor = PhaseEnableMotor(phase=19, enable=26)
     while True:
         for e in pygame.event.get():
             if e.type == pygame.JOYAXISMOTION:
                 throttle = -js.get_axis(1)  # throttle input: -1: max forward, 1: max backward
                 print(throttle)
         if throttle > 0:
-            GPIO.output(DIR_PIN, GPIO.LOW)  # forward
-            pwm.ChangeDutyCycle(throttle * 80)
+            motor.forward(throttle)
         elif throttle < 0:
-            GPIO.output(DIR_PIN, GPIO.HIGH)  # backward
-            throttle = -throttle
-            pwm.ChangeDutyCycle(throttle * 80)
+            motor.backward(throttle)
         else:
-            pwm.ChangeDutyCycle(0)
-                #steer = -js.get_axis(3)  # steer_input: -1: left, 1: right
-       
-        # ang = 90 * (1 + steer)
-        # if ang > 180:
-        #     ang = 180
-        # elif ang < 0:
-        #     ang = 0
-        # servo.angle = ang
+            motor.close()
         
 except KeyboardInterrupt:
     pygame.quit()
