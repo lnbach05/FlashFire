8a9f87b347e67c21e109ac4e92d0fd8bed70244f
HarrisonBounds <harrison.bounds777@gmail.com>
2023-09-29 11:15:06 -0500

    non continuous js input

--- train_and_deploy/components_tests/js_test.py
+++ train_and_deploy/components_tests/js_test.py
@@ -4,31 +4,36 @@ import os
 
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 
-# Initialize Pygame
-pygame.init()
-
-# Initialize the joystick (controller)
+# Initialize only the joystick module
 pygame.joystick.init()
+
+# Check if any joysticks/controllers are available
+if pygame.joystick.get_count() == 0:
+    print("No controllers available")
+    sys.exit(1)
 
 # Get the first joystick (change index if needed)
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
-# Main loop
+prev_button_states = [False] * joystick.get_numbuttons()
+
 try:
     while True:
         pygame.event.pump()  # Process events
 
-        # Get controller input values
-        axes = [joystick.get_axis(i) for i in range(joystick.get_numaxes())]
-        buttons = [joystick.get_button(i) for i in range(joystick.get_numbuttons())]
+        # Get current button states
+        button_states = [joystick.get_button(i) for i in range(joystick.get_numbuttons())]
 
-        # Display input values in the terminal
-        print("Axes:", axes)
-        print("Buttons:", buttons)
+        # Check for button press and release events
+        for button_index in range(len(button_states)):
+            if button_states[button_index] != prev_button_states[button_index]:
+                if button_states[button_index]:
+                    print(f"Button {button_index} pressed")
+                else:
+                    print(f"Button {button_index} released")
 
-        # Wait briefly before checking input again (adjust as needed)
-        pygame.time.delay(100)
+        prev_button_states = button_states
 
 except KeyboardInterrupt:
     pygame.quit()
