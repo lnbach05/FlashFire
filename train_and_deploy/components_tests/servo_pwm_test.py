5b1ac99f4f21950c760ccab84830144e17e1a6ee
HarrisonBounds <harrison.bounds777@gmail.com>
2023-10-13 14:21:45 -0500

    int to float

--- train_and_deploy/components_tests/servo_pwm_test.py
+++ train_and_deploy/components_tests/servo_pwm_test.py
@@ -4,7 +4,7 @@ servo = Servo(24)
 
 try:
     while True:
-        servo_value = input("Please enter a value between -1 and 1: ")
+        servo_value = float(input("Please enter a value between -1 and 1: "))
         servo.value = servo_value
 except KeyboardInterrupt:
     print("Done")
