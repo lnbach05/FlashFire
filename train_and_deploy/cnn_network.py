d7db95258001197441f5f8df5804ba20e2c542c4
Harrison Bounds <harrison.bounds777@gmail.com>
2023-12-05 16:09:51 -0600

    tune network

--- train_and_deploy/cnn_network.py
+++ train_and_deploy/cnn_network.py
@@ -7,8 +7,8 @@ class hblNet(nn.Module):
         
         #Convolution Layers
         self.conv_1 = nn.Conv2d(3, 32, kernel_size=(5, 5), stride=(2, 2))
-        self.conv_2 = nn.Conv2d(32, 64, kernel_size=(5, 5), stride=(2, 2))
-        self.conv_3 = nn.Conv2d(64, 82, kernel_size=(3, 3), stride=(2, 2))
+        self.conv_2 = nn.Conv2d(32, 64, kernel_size=(3, 3), stride=(2, 2))
+        # self.conv_3 = nn.Conv2d(64, 82, kernel_size=(3, 3), stride=(2, 2))
         # self.conv_4 = nn.Conv2d(48, 60, kernel_size=(3, 3), stride=(2, 2))
         # self.conv_5 = nn.Conv2d(60, 72, kernel_size=(3, 3), stride=(1, 1))
         # self.conv_6 = nn.Conv2d(72, 84, kernel_size=(3, 3), stride=(1, 1))
@@ -17,16 +17,16 @@ class hblNet(nn.Module):
         size_input = (width, height)  # output size: (input_size + 2*padding_size - kernel_size) / stride_size + 1
         size_1 = (int((size_input[0] - 5) / 2 + 1), int((size_input[1] - 5) / 2 + 1))
         size_2 = (int((size_1[0] - 5) / 2 + 1), int((size_1[1] - 5) / 2 + 1))
-        size_3 = (int((size_2[0] - 3) / 2 + 1), int((size_2[1] - 3) / 2 + 1))
+        # size_3 = (int((size_2[0] - 3) / 2 + 1), int((size_2[1] - 3) / 2 + 1))
         # size_4 = (int((size_3[0] - 3) / 2 + 1), int((size_3[1] - 3) / 2 + 1))
         # size_5 = (int(size_4[0] - 3 + 1), int(size_4[1] - 3 + 1))
         # size_6 = (int(size_5[0] - 3 + 1), int(size_5[1] - 3 + 1))
-        size_fc_input = size_3[0] * size_3[1] * 82
+        size_fc_input = size_2[0] * size_2[1] * 64
 
         #Fully Connected Layers
-        self.fc1 = nn.Linear(size_fc_input, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 2)
+        self.fc1 = nn.Linear(size_fc_input, 64)
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Linear(32, 2)
         self.relu = nn.ReLU()
         self.flatten = nn.Flatten()
 
