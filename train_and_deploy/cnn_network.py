8ab0c90d18e34f207aef05884153d763166b9e04
HarrisonBounds <harrison.bounds777@gmail.com>
2023-12-05 14:46:26 -0600

    larger feature size and reduced layer sizes for hblNet

--- train_and_deploy/cnn_network.py
+++ train_and_deploy/cnn_network.py
@@ -6,27 +6,27 @@ class hblNet(nn.Module):
         super().__init__()
         
         #Convolution Layers
-        self.conv_1 = nn.Conv2d(3, 24, kernel_size=(5, 5), stride=(2, 2))
-        self.conv_2 = nn.Conv2d(24, 36, kernel_size=(5, 5), stride=(2, 2))
-        self.conv_3 = nn.Conv2d(36, 48, kernel_size=(5, 5), stride=(2, 2))
-        self.conv_4 = nn.Conv2d(48, 60, kernel_size=(3, 3), stride=(2, 2))
-        self.conv_5 = nn.Conv2d(60, 72, kernel_size=(3, 3), stride=(1, 1))
-        self.conv_6 = nn.Conv2d(72, 84, kernel_size=(3, 3), stride=(1, 1))
+        self.conv_1 = nn.Conv2d(3, 32, kernel_size=(5, 5), stride=(2, 2))
+        self.conv_2 = nn.Conv2d(32, 64, kernel_size=(5, 5), stride=(2, 2))
+        self.conv_3 = nn.Conv2d(64, 82, kernel_size=(3, 3), stride=(2, 2))
+        # self.conv_4 = nn.Conv2d(48, 60, kernel_size=(3, 3), stride=(2, 2))
+        # self.conv_5 = nn.Conv2d(60, 72, kernel_size=(3, 3), stride=(1, 1))
+        # self.conv_6 = nn.Conv2d(72, 84, kernel_size=(3, 3), stride=(1, 1))
 
         #Compute Sizes
         size_input = (width, height)  # output size: (input_size + 2*padding_size - kernel_size) / stride_size + 1
         size_1 = (int((size_input[0] - 5) / 2 + 1), int((size_input[1] - 5) / 2 + 1))
         size_2 = (int((size_1[0] - 5) / 2 + 1), int((size_1[1] - 5) / 2 + 1))
-        size_3 = (int((size_2[0] - 5) / 2 + 1), int((size_2[1] - 5) / 2 + 1))
-        size_4 = (int((size_3[0] - 3) / 2 + 1), int((size_3[1] - 3) / 2 + 1))
-        size_5 = (int(size_4[0] - 3 + 1), int(size_4[1] - 3 + 1))
-        size_6 = (int(size_5[0] - 3 + 1), int(size_5[1] - 3 + 1))
-        size_fc_input = size_6[0] * size_6[1] * 84
+        size_3 = (int((size_2[0] - 3) / 2 + 1), int((size_2[1] - 3) / 2 + 1))
+        # size_4 = (int((size_3[0] - 3) / 2 + 1), int((size_3[1] - 3) / 2 + 1))
+        # size_5 = (int(size_4[0] - 3 + 1), int(size_4[1] - 3 + 1))
+        # size_6 = (int(size_5[0] - 3 + 1), int(size_5[1] - 3 + 1))
+        size_fc_input = size_3[0] * size_3[1] * 82
 
         #Fully Connected Layers
-        self.fc1 = nn.Linear(size_fc_input, 84)
-        self.fc2 = nn.Linear(84, 42)
-        self.fc3 = nn.Linear(42, 2)
+        self.fc1 = nn.Linear(size_fc_input, 64)
+        self.fc2 = nn.Linear(32, 32)
+        self.fc3 = nn.Linear(32, 2)
         self.relu = nn.ReLU()
         self.flatten = nn.Flatten()
 
@@ -34,9 +34,9 @@ class hblNet(nn.Module):
         x = self.relu(self.conv_1(x))  
         x = self.relu(self.conv_2(x))  
         x = self.relu(self.conv_3(x))  
-        x = self.relu(self.conv_4(x))  
-        x = self.relu(self.conv_5(x)) 
-        x = self.relu(self.conv_6(x))
+        # x = self.relu(self.conv_4(x))  
+        # x = self.relu(self.conv_5(x)) 
+        # x = self.relu(self.conv_6(x))
 
         x = self.flatten(x)
         x = self.relu(self.fc1(x))
@@ -85,5 +85,16 @@ class DonkeyNet(nn.Module):
         x = self.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+    
+
+h = hblNet(120, 160)
+d = DonkeyNet(120, 160)
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+print("HBL NET: ", count_parameters(h))
+print("Donkey NET: ", count_parameters(d))
+
 
 
