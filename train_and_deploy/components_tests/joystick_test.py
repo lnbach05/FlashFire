3e139b741e7ff25664ffc5b995d9d7df2302ddf3
lawan <ldjibrilhamidou@cub.uca.edu>
2024-01-24 14:08:48 -0600

    added joystick test code from autobots repo

---
+++ train_and_deploy/components_tests/joystick_test.py
@@ -0,0 +1,17 @@
+import pygame
+from gpiozero import AngularServo
+
+pygame.display.init()
+pygame.joystick.init()
+js = pygame.joystick.Joystick(0)
+
+servo = AngularServo(17, min_angle=-90, max_angle=90)
+
+while True:
+    #Main program
+    for e in pygame.event.get():
+        if e.type == pygame.JOYAXISMOTION:
+            #Get joystick steeing angle value from joystick and save it to a variable 
+            steer = -js.get_axis(3)  # steer_input: -1: left, 1: right
+    ang = -35 + steer * 55
+    servo.angle = ang
