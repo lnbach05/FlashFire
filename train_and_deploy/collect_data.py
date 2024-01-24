e328a3c2b782617850c6c5b8c827644d181d1449
Enguyen48 <78653474+Enguyen48@users.noreply.github.com>
2024-01-24 15:35:12 -0600

    Update collect_data.py

--- train_and_deploy/collect_data.py
+++ train_and_deploy/collect_data.py
@@ -1,8 +1,7 @@
-
 # Collect training data using bluetooth controller
 # While driving, save images and joystick inputs
-
 #!/usr/bin/python3
+
 import sys
 import os
 import cv2 as cv
@@ -11,20 +10,10 @@ from gpiozero import Servo
 import pygame
 import csv
 from datetime import datetime
-
 from time import time
 
-
 # SETUP
-# dummy video driver
 os.environ["SDL_VIDEODRIVER"] = "dummy"
-# load configs
-# config_path = os.path.join(sys.path[0], "config.json")
-# f = open(config_path)
-# data = json.load(f)
-# steering_trim = -1 * data['steering_trim']
-# throttle_lim = data['throttle_lim']
-# init servo controller
 image_dir = os.path.join(sys.path[0], 'data', datetime.now().strftime("%Y_%m_%d_%H_%M"), 'images/')
 if not os.path.exists(image_dir):
     try:
@@ -33,6 +22,7 @@ if not os.path.exists(image_dir):
         if e.errno != errno.EEXIST:
             raise
 label_path = os.path.join(os.path.dirname(os.path.dirname(image_dir)), 'labels.csv')
+
 # init controller
 pygame.display.init()
 pygame.joystick.init()
@@ -46,11 +36,6 @@ frame_counts = 0
 # init camera
 cap = cv.VideoCapture(0)
 cap.set(cv.CAP_PROP_FPS, 20)
-# for i in reversed(range(60)):  # warm up camera
-#     if not i % 20:
-#         print(i/20)
-#     ret, frame = cap.read()
-# # init timer, uncomment if you are cuious about frame rate
 start_stamp = time()
 ave_frame_rate = 0.
 start_time=datetime.now().strftime("%Y_%m_%d_%H_%M_")
@@ -73,13 +58,7 @@ try:
             if e.type == pygame.JOYAXISMOTION:
                 throttle = (-js.get_axis(1)) * 0.5 # throttle input: -1: max forward, 1: max backward
                 steer = -js.get_axis(2)  # steer_input: -1: left, 1: right
-            # elif e.type == pygame.JOYBUTTONDOWN:
-            #     if pygame.joystick.Joystick(0).get_button(0):
-            #         is_recording = not is_recording
-            #         if is_recording:
-            #             print("Recording data")
-            #         else:
-            #             print("Stopping data logging")
+
         if throttle > 0:
             motor.forward(throttle)
         elif throttle < 0:
@@ -95,7 +74,7 @@ try:
             servo.value = steer
 
         action = [steer, throttle]
-        #print(f"action: {action}")
+
         if is_recording:
             frame = cv.resize(frame, (120, 160))
             cv.imwrite(image_dir + str(frame_counts)+'.jpg', frame) # changed frame to gray
@@ -108,7 +87,6 @@ try:
         # monitor frame rate
         duration_since_start = time() - start_stamp
         ave_frame_rate = frame_counts / duration_since_start
-        #print(f"frame rate: {ave_frame_rate}")
 
         if cv.waitKey(1)==ord('q'):
             cv.destroyAllWindows()
